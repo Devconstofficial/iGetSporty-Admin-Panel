0ad7cf442481a47b77f32940f785dc798d3f8340
kulsoom26 <kulsoomkhurshid26@gmail.com>
2024-12-19 12:30:36 +0500

    Committing: "gh-pages"

--- android/app/src/main/kotlin/com/example/iget_sporty_admin_panel/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.iget_sporty_admin_panel
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/utils/app_strings.dart
+++
@@ -1,9 +0,0 @@
-String kAppName = 'iGetSporty Admin Panel';
-const String kSideBarScreenRoute = "/side-bar-screen-route";
-const String kDashboardScreenRoute = "/dashboard-screen-route";
-const String kVenuesOwnerScreenRoute = "/venues-owner-screen-route";
-const String kRevnueScreenRoute = "/revenue-screen-route";
-const String kUsersScreenRoute = "/users-screen-route";
-const String kBookingsScreenRoute = "/bookings-screen-route";
-const String kUserDetailsScreenRoute = "/UserDetailsScreenRoute";
-const String kBookingDetailsScreenRoute = "/BookingDetailsScreenRoute";
